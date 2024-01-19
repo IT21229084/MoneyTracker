906f8aa00ae9191ebd239163bd986798168ac416
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2024-01-19 11:36:47 +0530

    Client side is Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className="basic">
+          <input type="text" placeholder="+200 Rice" />
+          <input type="datetime-local" />
+        </div>
+        <div className="description">
+          <input type="text" placeholder="description" />
+        </div>
+        <button>Add New Transcations</button>
+      </form>
+      <div className="transcations">
+        <div className="transcation">
+          <div className="left">
+            <div className="name">New Mobile phone</div>
+            <div className="des">This is my first Mobile Phone.</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">2024-01-10 18:10</div>
+          </div>
+        </div>
+      </div>
+      <div className="transcations">
+        <div className="transcation">
+          <div className="left">
+            <div className="name">New Mobile phone</div>
+            <div className="des">This is my first Mobile Phone.</div>
+          </div>
+          <div className="right">
+            <div className="pricered">-$500</div>
+            <div className="datetime">2024-01-10 18:10</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
